e879f6021d2517d3116731eb59cb2c76039f26d0
kristinescloud <kristine.uchendu@gmail.com>
2025-01-01 16:39:12 -0500

    feat: Add remaining routes for the rest of the necessary API endpoints.

--- backend/src/routes/bookRoutes.ts
+++ backend/src/routes/bookRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { addBook, listAllBooks } from "../controllers/bookController";
+import { addBook, listAllBooks, searchBooks, viewBookDetails, updateBookDetails, removeBook } from "../controllers/bookController";
 
 const router = express.Router();
 
 router.post('/books', addBook);
-router.get('/oooks', listAllBooks);
+router.get('/books', listAllBooks);
+router.get('/books/search', searchBooks);
+router.get('/books/:bookID', viewBookDetails);
+router.put('/books/:bookID', updateBookDetails);
+router.delete('/books/:bookID', removeBook);
 
 export default router;
