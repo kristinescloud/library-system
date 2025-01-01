cc2ddd3bec9fdd060c5b98bf253c2c0812f7d637
kristinescloud <kristine.uchendu@gmail.com>
2025-01-01 18:22:06 -0500

    feat: Add remaining operations needed, for Node.js application.

--- backend/src/controllers/bookController.ts
+++ backend/src/controllers/bookController.ts
@@ -2,11 +2,12 @@ import { Request, Response } from "express";
 import { Book } from "../models/book";
 import { error } from "console";
 import { Error } from "mongoose";
+import { escape } from "querystring";
 
 export const addBook = async (req: Request, res: Response) => {
     try {
         const book = await Book.create(req.body);
-        res.status(201).json(book);
+        res.status(200).json(book);
     } catch (err: any) {
         res.status(400).json({ error: err.message });
     }
@@ -17,6 +18,117 @@ export const listAllBooks = async (req: Request, res: Response) => {
         const books = await Book.find();
         res.status(200).json(books);
     } catch (err: any) {
-        res.status(500).json({ error: err.message });
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// query parameters – criteria: String, keyword: String
+export const searchBooks = async (req: Request, res: Response) => {
+    const { criteria, keyword } = req.query;
+
+    if (!criteria || !keyword) {
+        res.status(401).json({ error: 'Criteria and keyword are needed' });
+        return;
+    }
+
+    const searchQuery: { [key: string]: string | RegExp } = {};
+    searchQuery[criteria as string] = new RegExp(keyword as string, 'i');
+
+    try {
+        const books = await Book.find(searchQuery);
+        res.status(200).json(books);
+    } catch (err: any) {
+        res.status(400).json({ error: err.message })
+    }
+}; 
+
+// request parameters – bookID: String
+export const viewBookDetails = async (req: Request, res: Response) => {
+    const { bookID } = req.params;
+
+    try {
+        let book;
+        switch (bookID.length) {
+            case 13:
+                book = await Book.findOne({ 'isbn.isbn13': bookID });
+            case 10:
+                book = await Book.findOne({ 'isbn.isbn10': bookID });
+            case 9:
+                book = await Book.findOne({ 'isbn.sbn': bookID });
+            default:
+                book = await Book.findOne({ 'isbn.other': bookID });
+        }
+        if (!book) {
+            res.status(404).json({ error: 'Book not found' });
+            return;
+        }
+        res.status(200).json(book);
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+}; 
+
+// request parameters – bookID: String, bookDetails: BookDetails
+export const updateBookDetails = async (req: Request, res: Response) => {
+    const { bookID } = req.params;
+    const updatedDetails = req.body;
+
+    try {
+        let book;
+        switch (bookID.length) {
+            case 13:
+                book = await Book.findOneAndUpdate(
+                    { 'isbn.isbn13': bookID },
+                    updatedDetails,
+                    { new: true, runValidators: true });
+            case 10:
+                book = await Book.findOneAndUpdate(
+                    { 'isbn.isbn10': bookID },
+                    updatedDetails,
+                    { new: true, runValidators: true });
+            case 9:
+                book = await Book.findOneAndUpdate(
+                    { 'isbn.sbn': bookID },
+                    updatedDetails,
+                    { new: true, runValidators: true });
+            default:
+                book = await Book.findOneAndUpdate(
+                    { 'isbn.other': bookID },
+                    updatedDetails,
+                    { new: true, runValidators: true});
+        }
+        if (!book) {
+            res.status(404).json({ error: 'Book not found' });
+            return;
+        }
+        res.status(200).json(book);
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+// request parameters – bookID: String
+export const removeBook = async (req: Request, res: Response) => {
+    const { bookID } = req.params;
+
+    try {
+        let book;
+        switch (bookID.length) {
+            case 13:
+                book = await Book.findOneAndDelete({ 'isbn.isbn13': bookID });
+            case 10:
+                book = await Book.findOneAndDelete({ 'isbn.isbn10': bookID });
+            case 9:
+                book = await Book.findOneAndDelete({ 'isbn.sbn': bookID });
+            default:
+                book = await Book.findOneAndDelete({ 'isbn.other': bookID });
+        }
+        if (!book) {
+            res.status(404).json({ error: 'Book not found' });
+            return;
+        }
+        res.status(201).json({ message: 'Book removed successfully' });
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
     }
 };
