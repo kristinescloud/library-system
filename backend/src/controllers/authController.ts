8240cce02cf454e7bae66bb1fe6fe2e1254de470
kristinescloud <kristine.uchendu@gmail.com>
2025-01-01 23:57:57 -0500

    feat: Create new file and write aunthentication-related logic.

---
+++ backend/src/controllers/authController.ts
@@ -0,0 +1,33 @@
+import { Request, Response } from 'express';
+import { User } from '../models/user';
+import { hashPassword, comparePassword, generateToken } from '../utils/auth';
+
+export const registerPatron = async (req: Request, res: Response) => {
+    const { id, username, password } = req.body;
+
+    try {
+        const hashedPassword = await hashPassword(password);
+        const user = await User.create({ _id: id, username, password: hashedPassword, role: 'patron' });
+
+        res.status(201).json({ message: 'User registered successfully.' });
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+export const loginUser = async (req: Request, res: Response) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username });
+
+        if (!user || !(await comparePassword(password, user.password))) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        const token = generateToken({ id: user._id, role: user.role });
+        res.status(200).json({ token });
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+}
--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -16,7 +16,7 @@ export const hashPassword = async (password: string) => {
     return await bcrypt.hash(password, 10);
 };
 
-export const comparePAssword = async (password: string, hashedPassword: string) => {
+export const comparePassword = async (password: string, hashedPassword: string) => {
     return await bcrypt.compare(password, hashedPassword);
 };
 
