398f703b697bf3359a2daa9862e74f0379c7d90d
kristinescloud <kristine.uchendu@gmail.com>
2025-01-01 23:22:23 -0500

    feat: Add helper functions for authentication.

---
+++ backend/src/utils/auth.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+import { Secret, PrivateKey } from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+const SECRET_KEY = process.env.TOKEN_SECRET as Secret;
+
+export const generateToken = (user: { id: string; role: string }) => {
+    return jwt.sign(user, SECRET_KEY, { expiresIn: '1h' });
+};
+
+export const verifyToken = (token: string) => {
+    return jwt.verify(token, SECRET_KEY);
+};
+
+export const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 10);
+};
+
+export const comparePAssword = async (password: string, hashedPassword: string) => {
+    return await bcrypt.compare(password, hashedPassword);
+};
+
