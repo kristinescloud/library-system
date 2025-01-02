5a2a6f8b523ffc74bf1c6bcc7577abc2a13e8336
kristinescloud <kristine.uchendu@gmail.com>
2025-01-01 23:29:04 -0500

    feat: Modify User model, and add clarifying comments.

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+    _id: { type: String, required: true }, // 14-digit number will be patron ID and 9-digit number will be librarian ID
     username: { type: String, required: true, unique: true },
-    idNumber: { type: String, required: true },
     password: { type: String, required: true },
     role: { type: String, enum: ['patron', 'librarian'], required: true},
 });
